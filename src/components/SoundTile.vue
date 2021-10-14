5cfa306406e5fa9ae9d771f6e1a67dbad6894f6a
Lars Poort <larspoort@Larss-MacBook-Pro.local>
2021-10-14 13:46:57 +0200

    intitial commit

---
+++ src/components/SoundTile.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+    <div id="app" class="flex items-center justify-center">
+        <div @click="playSound()" class='tegel'>
+            <img id="image" src="../assets/placeholder.jpg" alt="test">
+            <div id="playbutton"></div>
+            <h2 id="subtitle">{{ title }}</h2>
+            </div>
+     </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: [
+        'soundFile',
+        'title'
+    ],
+    data() {
+        return {
+            myTrack: null,
+        }
+    },
+    methods: {
+        playSound(){
+            this.myTrack.play()
+            this.soundEffect = true
+        }
+    },
+    mounted() {
+        this.myTrack = new Audio(this.soundFile)
+    }
+}
+</script>
+
