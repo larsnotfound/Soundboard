33d0f38893f91ac071fca22d00121765e52704f5
Lars Poort <larspoort@Larss-MacBook-Pro.local>
2021-11-12 21:43:17 +0100

    ‘master  git add . && git commit -m ‘intitial

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,11 @@
   <h1>Ronalds meest <mark>iconische</mark> uitspraken!</h1>
   <div class= "flex-container">
     <sound-tile :soundFile="audio" title="Goeiedag eem, moi"/>
-    <sound-tile :soundFile="audio" title="Goeiedag eem, moi"/>
-    <sound-tile :soundFile="audio" title="Goeiedag eem, moi"/>
-    <sound-tile :soundFile="audio" title="Goeiedag eem, moi"/>
-    <sound-tile :soundFile="audio" title="Goeiedag eem, moi"/>
-    <sound-tile :soundFile="audio" title="Goeiedag eem, moi"/>
+    <sound-tile :soundFile="vaatwasser" title="Vaatwasser" imageURL="imageURL"/>
+    <sound-tile :soundFile="audio" title="Content"/>
+    <sound-tile :soundFile="audio" title="Content"/>
+    <sound-tile :soundFile="audio" title="Content"/>
+    <sound-tile :soundFile="audio" title="Content"/>
     <sound-tile :soundFile="audio" title="Goeiedag eem, moi"/>
     <sound-tile :soundFile="audio" title="Goeiedag eem, moi"/>
   </div>
@@ -14,7 +14,7 @@
 
 <script>
  
- import SoundTile from './components/SoundTile.vue'
+import SoundTile from './components/SoundTile.vue'
 import audio from './assets/audio.mp3'
 export default {
   components: {
@@ -22,79 +22,9 @@ export default {
   },
   data() {
     return {
-      audio
+      audio,
+      imageURL: '../assets/placeholder.jpg'
     }
   }
 }
 </script>
-
-<style>
-
- @import url('https://fonts.googleapis.com/css2?family=Urbanist:wght@400;700&display=swap');
-
-body {
-    margin: 80px;
-}
-
-h1 {
-    font-family: 'Urbanist', sans-serif;
-    font-size: 36pt;
-    font-weight: 400;
-}
-
-mark {
-   background: none;
-   color: orange;
-   font-weight: 900;
-}
-
-.tegel {
-    max-width: 350px;
-    margin: 0 auto;
-    display: flex;
-    flex-direction: column;
-    background-color: orange;
-    display: inline-block;
-    padding: 0px;
-    border-radius: 10px;
-    margin: 40px 0px 0px 0px;
-}
-
-#image {
-    display: block;
-    width: 100%;
-    height: auto;
-    padding: 0;
-    border-radius: 10px 10px 0px 0px;
-}
-
-.tegel:hover {
-    opacity: 90%;
-}
-
-#subtitle {
-    color: black;
-    font-family: 'Urbanist', sans-serif;
-    font-size: 21pt;
-    font-weight: 800;
-    margin: 20px 20px 40px 20px;
-}
-
-#playbutton {
-    background-color: white;
-    border-radius: 50%;
-    width: 100px;
-    height: 100px;
-    z-index: 1000;
-    display: inline-flex;
-    margin-left: 20px;
-    margin-top: -50px;
-}
-
-.flex-container {
-    display: flex;
-    gap: 40px;
-    flex-wrap: wrap-reverse;
-}
-
-</style>
--- src/components/SoundTile.vue
+++ src/components/SoundTile.vue
@@ -2,19 +2,27 @@
     <div>
     <div id="app" class="flex items-center justify-center">
         <div @click="playSound()" class='tegel'>
-            <img id="image" src="../assets/placeholder.jpg" alt="test">
-            <div id="playbutton"></div>
+            <img id="image" src='../assets/ronald.gif'>
+            <div id="playbutton">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clip-rule="evenodd" />
+            </svg>
+            </div>
             <h2 id="subtitle">{{ title }}</h2>
             </div>
      </div>
     </div>
 </template>
 
+
+
+
+
 <script>
 export default {
     props: [
         'soundFile',
-        'title'
+        'title',
     ],
     data() {
         return {
