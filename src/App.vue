8b64aeb6f88b71e500b52a872a98a59f47644e0f
Lars Poort <larspoort@larss-mbp.wifi.nhlstenden.com>
2021-11-19 14:30:05 +0100

    ‘initial’

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <h1>Ronalds meest <mark>iconische</mark> uitspraken!</h1>
   <div class= "flex-container">
-    <sound-tile :soundFile="audio.mp3" title="Goeiedag eem, moi" imageURL="./placeholder.jpg"/>
-    <sound-tile :soundFile="vaatwasser" title="Vaatwasser" imageURL="./vaatwasser.png"/>
-    <sound-tile :soundFile="audio" title="Content" imageURL="./gonald.png"/>
-    <sound-tile :soundFile="audio" title="Content" imageURL="./verbazing.png"/>
-    <sound-tile :soundFile="audio" title="Content" imageURL="./ronnie.png"/>
-    <sound-tile :soundFile="audio" title="Content" imageURL="./flower.png"/>
-    <sound-tile :soundFile="audio" title="Goeiedag eem, moi" imageURL="./lach.png"/>
-    <sound-tile :soundFile="audio" title="Goeiedag eem, moi" imageURL="./hond.png"/>
+    <sound-tile soundFile="./audio.mp3" title="Goeiedag eem, moi" imageURL="./placeholder.jpg"/>
+    <sound-tile soundFile="./lekker_man.mp3" title="Lekker man" imageURL="./lekker_man.png"/>
+    <sound-tile soundFile="audio" title="Content" imageURL="./vaatwasser.png"/>
+    <sound-tile soundFile="audio" title="Content" imageURL="./verbazing.png"/>
+    <sound-tile soundFile="audio" title="Content" imageURL="./ronnie.png"/>
+    <sound-tile soundFile="audio" title="Content" imageURL="./flower.png"/>
+    <sound-tile soundFile="audio" title="Goeiedag eem, moi" imageURL="./lach.png"/>
+    <sound-tile soundFile="audio" title="Goeiedag eem, moi" imageURL="./hond.png"/>
   </div>
 </template>
 
 <script>
- 
+
 import SoundTile from './components/SoundTile.vue'
 import audio from './components/SoundTile.vue'
 export default {
